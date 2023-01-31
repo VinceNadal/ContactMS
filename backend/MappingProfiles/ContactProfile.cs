c3fc26252ddddb93dea1dea1fbaef4ee923ec5a0
Vincent Nadal <vnadal@fullscale.io>
2023-01-31 17:35:40 +0800

    Feature: Add MappingProfile for AutoMapper

---
+++ backend/MappingProfiles/ContactProfile.cs
@@ -0,0 +1,17 @@
+﻿using API.DTOs;
+using API.Models;
+using AutoMapper;
+
+namespace API.MappingProfiles
+{
+    public class ContactProfile : Profile
+    {
+        public ContactProfile()
+        {
+            CreateMap<Contact, ReadContactDto>().ReverseMap();
+            // Generate a Guid for the conversion of CreateContactDto to Contact class
+            CreateMap<CreateContactDto, Contact>().ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()));
+            CreateMap<UpdateContactDto, Contact>();
+        }
+    }
+}
